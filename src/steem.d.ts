e90161683728a33847de52ab67ebab1d81d583ea
anpigon <markan82@gmail.com>
2022-03-05 00:21:27 +0900

    Add steem.d.ts

---
+++ src/steem.d.ts
@@ -0,0 +1,14 @@
+declare module 'steem' {
+  module broadcast {
+    export function commentAsync(
+      postingWif: string,
+      parent_author: string,
+      parent_permlink: string,
+      username: string,
+      permlink: string,
+      title: string,
+      body: string,
+      json_metadata: { tags: string[]; app: string },
+    ): Promise<any>;
+  }
+}
