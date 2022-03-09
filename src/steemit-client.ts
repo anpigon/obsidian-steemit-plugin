05a536a5d570393ffcfa1e0db048bcb3e369c211
anpigon <markan82@gmail.com>
2022-03-09 17:35:33 +0900

    Change gray-matter to metadataCache.getFileCache & vault.read to vault.cachedRead

--- src/steemit-client.ts
+++ src/steemit-client.ts
@@ -1,5 +1,4 @@
 import { broadcast } from 'steem';
-import matter, { GrayMatterOption } from 'gray-matter';
 import { App, MarkdownView, Notice, parseFrontMatterTags } from 'obsidian';
 
 import SteemitPlugin from './main';
@@ -16,9 +15,13 @@ export class SteemitClient {
     const activeView = workspace.getActiveViewOfType(MarkdownView);
     if (activeView) {
       try {
-        const fileContent = await this.app.vault.read(activeView.file);
-        const parsedMatter = matter(fileContent);
-        const frontMatter: SteemitFrontMatter = parsedMatter.data;
+        const fileContent = await this.app.vault.cachedRead(activeView.file);
+        const frontMatter = this.app.metadataCache.getFileCache(activeView.file)
+          ?.frontmatter as SteemitFrontMatter;
+        if (!frontMatter) {
+          new Notice('Please write frontmatter.');
+          return;
+        }
 
         const tags = parseFrontMatterTags(frontMatter)?.map(tag =>
           tag.replace(/^#/, '').trim(),
@@ -40,7 +43,8 @@ export class SteemitClient {
           'kr';
 
         // Strip front-matter and HTML comments
-        const parsedContent = parsedMatter.content
+        const parsedContent = fileContent
+          .slice(frontMatter.position.end.offset)
           .replace(/^<!--.*-->$/ms, '')
           .trim();
 
