6e97f46dc3e47baa27100cc2b6d0120786308354
anpigon <markan82@gmail.com>
2022-03-28 22:50:39 +0900

    Fix types.ts

--- src/types.ts
+++ src/types.ts
@@ -14,9 +14,13 @@ export interface SteemitPluginSettings {
   category: string;
   username: string;
   password: string;
+  appName: string;
 }
 
 export interface SteemitJsonMetadata {
   app: string;
+  format: string;
   tags?: string[];
+  canonical_url?: string;
+  image?: string[];
 }
