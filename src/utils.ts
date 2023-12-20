2ef9d4d8f3ea5d486b238ee2fe5d38b1aa0c0fd1
anpigon <markan82@gmail.com>
2023-12-20 22:38:52 +0900

    fix(main.ts): remove unused import and variable declaration to improve code readability and maintainability feat(main.ts): refactor parsePostData function to use async/await and improve error handling feat(main.ts): refactor code to use parsed post object instead of separate variables for better code organization and readability fix(utils.ts): remove unused functions and imports to improve code cleanliness and maintainability feat(utils.ts): refactor makeDefaultPermlink function to be exported for reusability

--- src/utils.ts
+++ src/utils.ts
@@ -1,45 +1,10 @@
-import { MarkdownView, parseFrontMatterTags, parseYaml } from 'obsidian';
-import { SteemitFrontMatter, SteemitPost } from './types';
-
 export const frontmatterRegex = /^---\n(?:((?!---)(.|\n)*?)\n)?---(\n|$)/;
 
-export function parseFrontMatter(content: string): SteemitFrontMatter | undefined {
-  if (!frontmatterRegex.test(content)) {
-    return;
-  }
-  return frontmatterRegex
-    .exec(content)?.[1]
-    ?.split('\n')
-    .reduce<SteemitFrontMatter>((acc, x) => {
-      const [key, value] = x.split(':');
-      return (acc[key?.trim()] = value?.trim()), acc;
-    }, {} as SteemitFrontMatter);
-}
-
-export function parsePostData(activeView: MarkdownView): SteemitPost {
-  const frontMatter = parseFrontmatter(activeView.data);
-  return {
-    category: frontMatter?.category?.toString() || '',
-    permlink: frontMatter?.permlink?.toString() || makeDefaultPermlink(),
-    title: frontMatter?.title?.toString() || activeView.file?.basename,
-    tags: frontMatter?.tags?.toString() || '',
-    body: removeObsidianComments(stripFrontmatter(activeView.data)),
-  };
-}
-
-function makeDefaultPermlink() {
+export function makeDefaultPermlink() {
   return new Date()
     .toISOString()
     .replace(/[^\w]+/g, '')
     .toLowerCase();
-}
-
-export function parseFrontmatter(content: string) {
-  const results = parseYaml(content.split('---')?.[1] ?? '');
-  const tags = parseFrontMatterTags(results)
-    ?.map(tag => tag.replace(/^#/, '').trim())
-    .join(' ');
-  return { ...results, tags };
 }
 
 export function stripFrontmatter(content: string) {
