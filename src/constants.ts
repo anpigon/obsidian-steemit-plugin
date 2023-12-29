9cfc4f371dfb090a3ac7bb0179f52ddfcad1359e
anpigon <markan82@gmail.com>
2023-12-29 20:34:09 +0900

    refactor(constants.ts): add default settings and reward type options to improve code readability and maintainability refactor(submit_confirm_modal.ts): use reward type options from constants file to improve code reusability and maintainability

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,18 @@
+import { RewardType, SteemitPluginSettings } from './types';
+
 export const DEFAULT_FOOTER =
   '<center><sub>Posted using <a href="https://obsidian.md/plugins?id=obsidian-steemit" target="_blank">Obsidian Steemit plugin</a></sub></center>';
+
+export const DEFAULT_SETTINGS: SteemitPluginSettings = {
+  category: '',
+  username: '',
+  password: '',
+  appName: '',
+  rewardType: RewardType.DEFAULT,
+};
+
+export const RewardTypeOptions = [
+  { value: RewardType.SP, label: 'Power Up 100%' },
+  { value: RewardType.DEFAULT, label: 'Default (50% / 50%)' },
+  { value: RewardType.DP, label: 'Decline Payout' },
+];
