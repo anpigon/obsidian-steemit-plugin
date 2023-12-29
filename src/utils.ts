69e98e3acd2d6c6d82a1a0e50f793bd0851d7ba4
anpigon <markan82@gmail.com>
2023-12-29 22:00:15 +0900

    fix(main.ts): import missing functions from utils file to avoid undefined errors fix(main.ts): handle case when activeView or activeView.file is null in getActiveView() function fix(main.ts): handle error when publishing post and display error message feat(main.ts): update file content with new front matter and body after publishing post fix(utils.ts): import FrontMatterCache type from obsidian to fix type error feat(utils.ts): add functions to remove front matter, extract content body, and create new front matter

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { FrontMatterCache } from 'obsidian';
+
 export const frontmatterRegex = /^---\n(?:((?!---)(.|\n)*?)\n)?---(\n|$)/;
 
 export function makeDefaultPermlink() {
@@ -14,3 +16,40 @@ export function stripFrontmatter(content: string) {
 export function removeObsidianComments(content: string) {
   return content.replace(/^\n?%%(.+?)%%\n?$/gms, '');
 }
+
+export const removeFrontmatter = (markdownContent: string) => {
+  const frontmatterRegex = /^---\s*\n([\s\S]*?)\n---\s*\n/;
+  const match = markdownContent.match(frontmatterRegex);
+
+  if (match) {
+    // Extract frontmatter and remove it from the content
+    const frontmatterString = match[0];
+    const contentWithoutFrontmatter = markdownContent.replace(frontmatterRegex, '');
+
+    return { frontmatterString, contentWithoutFrontmatter };
+  } else {
+    // No frontmatter found
+    return { frontmatterString: '', contentWithoutFrontmatter: markdownContent };
+  }
+};
+
+// 파일 내용에서 프론트매터를 제거하는 함수
+export const extractContentBody = (fileContent: string): string => {
+  const { contentWithoutFrontmatter } = removeFrontmatter(fileContent);
+  return contentWithoutFrontmatter;
+};
+
+// 새로운 프론트매터를 생성하는 함수
+export const createNewFrontMatter = (
+  existingFrontMatter: FrontMatterCache,
+  additions: FrontMatterCache,
+): FrontMatterCache => {
+  const newFrontMatter = { ...existingFrontMatter, ...additions };
+  // 값이 없는 키 삭제
+  Object.entries(newFrontMatter).forEach(([key, value]) => {
+    if (!value) {
+      delete newFrontMatter[key];
+    }
+  });
+  return newFrontMatter;
+};
